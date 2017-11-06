ed348eef3b2768018f6300278a96fac30439fc6c
Hansel Qiu <Hansel@Hansels-Air.hitronhub.home>
2017-11-05 22:38:38 -0700

    adding c++ problems

---
+++ C++/keytocrypto.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	string input1;
+	string input2;
+
+	cin>> input1>>input2;
+
+	
+}
---
+++ Python/simpleaddition.py
@@ -0,0 +1,3 @@
+line1 = input()
+line2 = input()
+print (int(line1)+int(line2))
